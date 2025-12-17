a02946b1a6b7dc2b8fcea7f0b23a6b77846d7697
Joyce Wu <jianjoycewu@gmail.com>
2025-12-16 20:31:10 -0800

    some card game

--- src/hash_map/hm_with_rehash.py
+++ src/hash_map/hm_with_rehash.py
@@ -3,6 +3,6 @@ Rehashing process:
 1. create a new array of buckets (typically 2 * the current capacity)
 2. iterate through the old hash table
 3. for each key-value pair, recompute the hash : new_hash = key % new_capacity
-4. insert earch pair into the new array using the new index
+4. insert each pair into the new array using the new index
 5. discard the old array
 """
